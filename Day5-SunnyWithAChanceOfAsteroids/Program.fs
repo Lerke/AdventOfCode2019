9d49ecaefa46a40be96faab4f360ecc506e2fab6
Lerke <willemsporen@gmail.com>
2019-12-08 22:44:12 +0100

    Add Input Output instructions for task 1

--- Day5-SunnyWithAChanceOfAsteroids/Program.fs
+++ Day5-SunnyWithAChanceOfAsteroids/Program.fs
@@ -1,12 +1,34 @@
-﻿/// Possible operation types of our simple computer
-type OperationType = | Add = 1 | Multiply = 2 | Stop = 99
+﻿open System
+open System
+
+/// Possible operation types of our simple computer
+type OperationType =
+    | Add = 1
+    | Multiply = 2
+    | Store = 3
+    | Output = 4
+    | Stop = 99
 
 /// Transform an input number into one of the possible opcodes
 let getOperation num =
     match num with
     | 1 -> OperationType.Add
     | 2 -> OperationType.Multiply
+    | 3 -> OperationType.Store
+    | 4 -> OperationType.Output
     | 99 -> OperationType.Stop
+
+let rec readInput() =
+    printf "Input: "
+    let input = Console.ReadLine()
+    match Int32.TryParse(input) with
+    | true, i -> i
+    | false, _ ->
+        printfn "Try again"
+        readInput()
+
+let output value =
+    printfn "OUTPUT: %d" value
 
 /// Update the memory at index. Returns a new collection where index has been replaced with value.
 let updateMemoryLocation memory index value = List.mapi (fun i x -> if i = index then value else x ) memory
@@ -23,6 +45,14 @@ let rec calculateNext (memory:List<int>) ip =
          calculateNext (updateMemoryLocation
                             memory
                             (memory.Item(ip+3)) ((memory.Item(memory.Item(ip+1)) * (memory.Item(memory.Item(ip+2)))))) (ip + 4)
+    | OperationType.Store ->
+        let input = readInput()
+        calculateNext (updateMemoryLocation
+                            memory
+                            (memory.Item(ip+1)) (input)) (ip + 2)
+    | OperationType.Output ->
+        output (memory.Item(memory.Item(ip+1)))
+        calculateNext memory (ip + 2)
     | OperationType.Stop -> memory
     
 /// Start calculating until halt given a certain memory map. Assume Instruction Pointer starts at 0
@@ -50,7 +80,8 @@ let puzzleTwo input target =
 
 [<EntryPoint>]
 let main argv =
-    let input = [ 1;0;0;3;1;1;2;3;1;3;4;3;1;5;0;3;2;1;13;19;1;9;19;23;2;13;23;27;2;27;13;31;2;31;10;35;1;6;35;39;1;5;39;43;1;10;43;47;1;5;47;51;1;13;51;55;2;55;9;59;1;6;59;63;1;13;63;67;1;6;67;71;1;71;10;75;2;13;75;79;1;5;79;83;2;83;6;87;1;6;87;91;1;91;13;95;1;95;13;99;2;99;13;103;1;103;5;107;2;107;10;111;1;5;111;115;1;2;115;119;1;119;6;0;99;2;0;14;0 ];
-    puzzleOne input
-    puzzleTwo input 19690720
+    let input = [ 3;0;4;0;99 ]
+    let result = doCalculation input
+//    puzzleOne input
+//    puzzleTwo input 19690720
     0
